64da946aae31b5efc9c7054a56158a143bbc78a2
Anish <aws.project.assignment@gmail.com>
2022-12-06 19:13:19 -0500

    Initial commit

---
+++ models/pet.py
@@ -0,0 +1,11 @@
+from models.owner import User
+
+class pet:
+    _name= str()
+    _breed=str()
+    _age=int()
+    _gender=str()
+    _type=str()
+    _weight = float()
+    _address=str()
+    _owner=User()
